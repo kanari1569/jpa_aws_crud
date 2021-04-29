ff9703a815dad4dfa7391ad23f9f250e39a5bb0c
LSH <kanari1569@gmail.com>
2021-04-29 16:21:12 +0900

    JPA Auditing

---
+++ src/main/java/com/jpa_crud/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.jpa_crud.web.dto;
+
+import com.jpa_crud.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
